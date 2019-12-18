18314eb3e7785bce78cc9d92fbae5b77d3419848
Adam Mathieson <mathieson1222@gmail.com>
2019-12-17 21:47:07 -0800

    multiple tests passing, refactor

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import { add } from './App'
 
+// Unit test
+// It only tests one thing
 test('add', () => {
-    const value = add(1,2)
-    expect(value).toBe(3)
+    // const value = add(1,2)
+    // expect(value).toBe(3)
+    expect(add(1,2)).toBe(3)
+    expect(add(10,15)).toBe(25)
 })
 
