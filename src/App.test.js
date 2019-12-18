0ccb1314d4dc4b6111a4be607ab6fc2f10be5c3d
Adam Mathieson <mathieson1222@gmail.com>
2019-12-17 22:01:23 -0800

    integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { add } from './App'
+import { add, total } from './App'
 
 // Unit test
 // It only tests one thing
@@ -7,5 +7,9 @@ test('add', () => {
     // expect(value).toBe(3)
     expect(add(1,2)).toBe(3)
     expect(add(10,15)).toBe(25)
+})
+
+test('total', () => {
+    expect(total(5, 20)).toBe('$25')
 })
 
